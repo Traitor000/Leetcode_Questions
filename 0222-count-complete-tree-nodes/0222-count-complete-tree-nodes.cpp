70d0f22bef5c6e1926bd1fc2b8f1f45c7a9e8ad0
Ashutosh Gautam <75945709+Traitor000@users.noreply.github.com>
2022-11-15 17:51:19 +0530

    Time: 66 ms (50.78%), Space: 31 MB (9.34%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,14 +1,27 @@
+// class Solution {
+// public:
+//     int countNodes(TreeNode* root) {
+//         if (!root) return 0;
+        
+//         int l = 1, r = 1;
+//         TreeNode *ptr_r = root, *ptr_l = root;
+//         while (ptr_l = ptr_l->left) l++;
+//         while (ptr_r = ptr_r->right) r++;
+        
+//         if (l == r) return pow(2, l) - 1;
+//         return 1 + countNodes(root->left) + countNodes(root->right);
+//     }
+// };
 class Solution {
-public:
+    public:
     int countNodes(TreeNode* root) {
-        if (!root) return 0;
+        if(!root) return 0;
+        int l=1, r=1;
+        TreeNode* tempr=root, *templ=root;
+        while(templ=templ->left)l+=1;
+        while(tempr=tempr->right)r+=1;
+        if(l==r)return pow(2,l)-1;
+        return 1+countNodes(root->left)+countNodes(root->right);
         
-        int l = 1, r = 1;
-        TreeNode *ptr_r = root, *ptr_l = root;
-        while (ptr_l = ptr_l->left) l++;
-        while (ptr_r = ptr_r->right) r++;
-        
-        if (l == r) return pow(2, l) - 1;
-        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
